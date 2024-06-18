e551af11752bfbd1ecdd68d3c5254659b963cc80
JackCaolho <gustavoallify@gmail.com>
2024-06-18 08:33:38 -0300

    Download curriculo

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/download')
+def download_file():
+    return send_from_directory('static', 'GUSTAVO ALLIFY CURRICULO.pdf', as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
